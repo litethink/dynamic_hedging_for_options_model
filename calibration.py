9043e0b11fd9b7b7c93cfa1315a3a13d95c05e38
litethink <69028117+litethink@users.noreply.github.com>
2021-12-20 17:13:14 +0800

    Update calibration.py

--- calibration.py
+++ calibration.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import scipy.interpolate as sci
 from scipy.optimize import brute
 from scipy.optimize import fmin
 
@@ -8,3 +10,68 @@ r_list = np.array((-0.032, -0.013, -0.013, 0.007, 0.043,
 
 factors = (1 + t_list * r_list)
 zero_rates = 1 / t_list * np.log(factors)
+tck = sci.splrep(t_list, zero_rates, k=3)  # cubic splines
+tn_list = np.linspace(0.0, 1.0, 24)
+ts_list = sci.splev(tn_list, tck, der=0)
+de_list = sci.splev(tn_list, tck, der=1)
+f = ts_list + de_list * tn_list
+
+r0 = r_list[0]
+
+def CIR_forward_rate(opt):
+    ''' Function for forward rates in CIR85 model.
+
+    Parameters
+    ==========
+    kappa_r: float
+        mean-reversion factor
+    theta_r: float
+        long-run mean
+    sigma_r: float
+        volatility factor
+
+    Returns
+    =======
+    forward_rate: float
+        forward rate
+    '''
+    kappa_r, theta_r, sigma_r = opt
+    t = tn_list
+    g = np.sqrt(kappa_r ** 2 + 2 * sigma_r ** 2)
+    sum1 = ((kappa_r * theta_r * (np.exp(g * t) - 1)) /
+            (2 * g + (kappa_r + g) * (np.exp(g * t) - 1)))
+    sum2 = r0 * ((4 * g ** 2 * np.exp(g * t)) /
+                 (2 * g + (kappa_r + g) * (np.exp(g * t) - 1)) ** 2)
+    forward_rate = sum1 + sum2
+    return forward_rate
+
+#
+# Error Function
+#
+
+
+def CIR_error_function(opt):
+    ''' Error function for CIR85 model calibration. '''
+    kappa_r, theta_r, sigma_r = opt
+    if 2 * kappa_r * theta_r < sigma_r ** 2:
+        return 100
+    if kappa_r < 0 or theta_r < 0 or sigma_r < 0.001:
+        return 100
+    forward_rates = CIR_forward_rate(opt)
+    # import pdb
+    # pdb.set_trace()
+    MSE = np.sum((f - forward_rates) ** 2) / len(f)
+    # print opt, MSE
+    return MSE
+
+#
+# Calibration Procedure
+#
+def CIR_calibration():
+    opt = fmin(CIR_error_function, [1.0, 0.02, 0.1],
+               xtol=0.00001, ftol=0.00001,
+               maxiter=300, maxfun=500)
+    return opt
+
+
+
